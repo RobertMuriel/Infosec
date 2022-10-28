ab0dd7b7836c32261da16a00bb8dd2b7eb374094
RobertMuriel <116899445+RobertMuriel@users.noreply.github.com>
2022-10-28 11:26:49 -0500

    Version 1.0

--- Pivoting/NetScan.sh
+++ Pivoting/NetScan.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-for i in $(seq 1 254); do
-  timeout 1 bash -c "ping -c1 10.10.0.$i" &>/dev/null && echo "[+] HOST 10.10.0.$i - ACTIVE" &
-done; wait
+# Use: ./NetScan.sh 192.168.1
+
+if [ "$1" == "" ]
+then
+    echo "You forgot an IP address!"
+    echo "Syntax: ./NetScan.sh 192.168.1"
+else
+    for i in $(seq 1 254); do
+      timeout 1 bash -c "ping -c1 $1.$i" &>/dev/null && echo "[+] $1.$i - ACTIVE" &
+    done; wait
+fi
