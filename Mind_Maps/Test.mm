46b8fa4fee2be275a25f475a30b85dfec90080d6
RobertMuriel <116899445+RobertMuriel@users.noreply.github.com>
2022-12-21 19:27:05 -0600

    Delete Mind_Maps directory

--- Mind_Maps/Test.mm
+++
@@ -1,9 +0,0 @@
-<map version="1.0.1">
-<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1671481430724" ID="ID_1676812763" MODIFIED="1671481501628" TEXT="New Mind Map">
-<icon BUILTIN="full-1"/>
-<icon BUILTIN="full-1"/>
-<node CREATED="1671481520907" ID="ID_709926816" MODIFIED="1671481527918" POSITION="right" TEXT="Child Node 1"/>
-<node CREATED="1671481530334" ID="ID_353239561" MODIFIED="1671481536309" POSITION="left" TEXT="Child Node 2"/>
-</node>
-</map>
