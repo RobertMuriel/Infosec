9806a337471b9df2841b0adde417ef814588c05e
RobertMuriel <116899445+RobertMuriel@users.noreply.github.com>
2022-12-21 19:31:58 -0600

    Add files via upload

---
+++ Mapas_Mentales/Fundamentos_de_Ciberseguridad.mm
@@ -0,0 +1,212 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1671481430724" ID="ID_1676812763" MODIFIED="1671667721022" TEXT="Ciberseguridad">
+<font NAME="Arial" SIZE="20"/>
+<icon BUILTIN="password"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1671481530334" ID="ID_353239561" MODIFIED="1671581319863" POSITION="left" TEXT="Ciberdelito">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1671581152252" ID="ID_1378711492" MODIFIED="1671668048667" TEXT="Actividades delictivas realizadas con ayuda de redes o sistemas de informaci&#xf3;n electronicos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1671581555428" ID="ID_324137345" MODIFIED="1671581566737" TEXT="Delitos Tradicionales">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671581397950" ID="ID_1405056962" MODIFIED="1671581579479" TEXT="-Fraude">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1671581425721" ID="ID_1348106807" MODIFIED="1671668110200" TEXT="- Intercepci&#xf3;n de comunicaciones">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1671581378283" ID="ID_1836768350" MODIFIED="1671581613900" TEXT="- Pederastia">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671581624504" ID="ID_315310391" MODIFIED="1671668082929" TEXT="Delitos Nuevos (Por su objetivo)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671581448525" ID="ID_290236573" MODIFIED="1671668129073" TEXT="- Borrado de datos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1671581466310" ID="ID_425988419" MODIFIED="1671668149608" TEXT="- Interrupci&#xf3;n de servicios web">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1671581685717" ID="ID_1898865173" MODIFIED="1671667799427" POSITION="right" TEXT="Conjunto de tecnicas, sistemas de gestion y otras medidas que pretenden proteger de incidentes la informaci&#xf3;n y los medios que la tratan">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="Arial" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1671581862652" ID="ID_733376003" MODIFIED="1671581952911" POSITION="right" TEXT="Pretende proteger la informaci&#xf3;n sea cual sea su medio, papel o electronico o de forma de tratamiento manual o automatizado">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1671581974652" ID="ID_1411794028" MODIFIED="1671668199850" POSITION="left" TEXT="Propiedades de la Informaci&#xf3;n">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1671581991551" ID="ID_442719789" MODIFIED="1671582001954" TEXT="Confidencialidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671582018478" ID="ID_1042748560" MODIFIED="1671582044102" TEXT="Que la informacion solo sea revelada a los usuarios autorizados">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671582005428" ID="ID_1740239730" MODIFIED="1671582005428" TEXT="">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1671582051145" ID="ID_943598816" MODIFIED="1671668217260" TEXT="Integridad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671582061148" ID="ID_1799855112" MODIFIED="1671582076543" TEXT="Que la informaicon sea excacta y completa">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671582088663" ID="ID_1873139374" MODIFIED="1671582093143" TEXT="Disponibilidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671582100331" ID="ID_188447710" MODIFIED="1671582134633" TEXT="Que la informacion este disponible en tiempo y forma exclusivamente para los usuarios legitimados">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1671582153250" ID="ID_193615448" MODIFIED="1671582189475" POSITION="right" TEXT="Prop&#xed;edades Adicionales">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1671582209010" ID="ID_982879392" MODIFIED="1671582286668" TEXT="No repudio">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671582218547" ID="ID_1382836365" MODIFIED="1671668027463" TEXT="Se asegura que algo que sea realmente v&#xe1;lido no pueda ser rechazado o negado">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671582306921" ID="ID_1377894838" MODIFIED="1671582311383" TEXT="Trazabilidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671582313577" ID="ID_334901914" MODIFIED="1671582327621" TEXT="Que pueda rastrearse su ciclo de vida">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1671582420946" ID="ID_1219139494" MODIFIED="1671668461133" POSITION="left" TEXT="Factores en contra de las medidas de seguridad">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1671582436678" ID="ID_1723908660" MODIFIED="1671582447660" TEXT="Costo">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671668295139" ID="ID_1785277713" MODIFIED="1671668306382" TEXT="Se ve como gasto no como inversion">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671582465235" ID="ID_326713610" MODIFIED="1671582469071" TEXT="Rendimiento">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671668317210" ID="ID_1466252318" MODIFIED="1671668354658" TEXT="Implica mas procesos y puede afectar el rendimiento de la operaci&#xf3;n">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671582470675" ID="ID_511788174" MODIFIED="1671582485830" TEXT="Usabilidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671668426065" ID="ID_1669291109" MODIFIED="1671668436203" TEXT="Dificultan las tareas de los usuarios">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1671582510990" ID="ID_186948119" MODIFIED="1671582518199" POSITION="right" TEXT="Carencia de Expertos en esta materia">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1671668622363" ID="ID_1188107308" MODIFIED="1671668633438" POSITION="left" TEXT="Medidas de seguridad">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1671668635775" ID="ID_202911119" MODIFIED="1671668683515" TEXT="Tratan de mitigar los riesgos (eliminarlos es imposible)">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1671668699081" ID="ID_225812873" MODIFIED="1671668714792" TEXT="Controles o Contramedidas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1671668757766" ID="ID_541658055" MODIFIED="1671668765783" TEXT="Principio de proporcionalidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671668771538" ID="ID_1313902780" MODIFIED="1671668832171" TEXT="Deden ser proporcionales a los datos, los riesgos, los sistemas de informaci&#xf3;n y al estado de la tecnologia">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671668864694" ID="ID_268405190" MODIFIED="1671668872951" TEXT="Segun su actuaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671668875021" ID="ID_1544686827" MODIFIED="1671668880900" TEXT="Preventivas">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1671668938166" ID="ID_1504789239" MODIFIED="1671668994647" TEXT="Reducen probabilidad de un ataque"/>
+</node>
+<node COLOR="#990000" CREATED="1671668883227" ID="ID_274553372" MODIFIED="1671668921542" TEXT="De detecci&#xf3;n">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1671668965330" ID="ID_825783754" MODIFIED="1671669031245" TEXT="Reduce la propagaci&#xf3;n de un ataque"/>
+</node>
+<node COLOR="#990000" CREATED="1671668895558" ID="ID_258213955" MODIFIED="1671668900341" TEXT="Correctivas">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1671669039364" ID="ID_1232933552" MODIFIED="1671669050926" TEXT="Reduce el da&#xf1;o de un ataque"/>
+</node>
+<node COLOR="#990000" CREATED="1671668902378" ID="ID_1509047195" MODIFIED="1671668911457" TEXT="De recuperaci&#xf3;n">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1671669057581" ID="ID_301791969" MODIFIED="1671669072433" TEXT="Reduce las consecuencias de un ataque"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671669099423" ID="ID_857136177" MODIFIED="1671669099423" TEXT="">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671669100286" ID="ID_1269330338" MODIFIED="1671669110197" TEXT="Segun su naturaleza">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1671669136490" ID="ID_6686402" MODIFIED="1671669139119" TEXT="Fisicas">
+<node COLOR="#111111" CREATED="1671669172265" ID="ID_421876329" MODIFIED="1671669195240" TEXT="Prevencion de Incendios, inundaciones, temblores"/>
+</node>
+<node COLOR="#111111" CREATED="1671669213624" ID="ID_123555271" MODIFIED="1671669217253" TEXT="Tecnicas">
+<node COLOR="#111111" CREATED="1671669236692" ID="ID_273116474" MODIFIED="1671669251703" TEXT="Identificaci&#xf3;n y autenticaci&#xf3;n">
+<node COLOR="#111111" CREATED="1671669398679" ID="ID_146909328" MODIFIED="1671669431145" TEXT="Sistemas basados en contrase&#xf1;as, biometricos y doble factor"/>
+</node>
+<node COLOR="#111111" CREATED="1671669253674" ID="ID_1002082939" MODIFIED="1671669259234" TEXT="Confidencialidad">
+<node COLOR="#111111" CREATED="1671669446863" ID="ID_328327951" MODIFIED="1671669456488" TEXT="Mecanismos de cifrado"/>
+</node>
+<node COLOR="#111111" CREATED="1671669261840" ID="ID_231365923" MODIFIED="1671669267381" TEXT="Integridad">
+<node COLOR="#111111" CREATED="1671669479805" ID="ID_302867836" MODIFIED="1671669497353" TEXT="Mediante Hashes"/>
+</node>
+<node COLOR="#111111" CREATED="1671669270229" ID="ID_167259803" MODIFIED="1671669275898" TEXT="No repudio">
+<node COLOR="#111111" CREATED="1671669506877" ID="ID_3761224" MODIFIED="1671669540345" TEXT="Firma Electronica o Digital"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1671669565297" ID="ID_284337857" MODIFIED="1671669568388" TEXT="Legales">
+<node COLOR="#111111" CREATED="1671669581565" ID="ID_435831728" MODIFIED="1671669593702" TEXT="Protecci&#xf3;n de datos personales"/>
+<node COLOR="#111111" CREATED="1671669596311" ID="ID_30815665" MODIFIED="1671669618387" TEXT="C&#xf3;digos Penales"/>
+<node COLOR="#111111" CREATED="1671669622661" ID="ID_866914164" MODIFIED="1671669632765" TEXT="Firma Electronica"/>
+<node COLOR="#111111" CREATED="1671669638514" ID="ID_1080696822" MODIFIED="1671669652523" TEXT="Proteccion de infraestructuras criticas"/>
+</node>
+<node COLOR="#111111" CREATED="1671669680009" ID="ID_327965564" MODIFIED="1671669700352" TEXT="De Gestion">
+<node COLOR="#111111" CREATED="1671670029948" ID="ID_1235992789" MODIFIED="1671670036566" TEXT="ISO 27000"/>
+<node COLOR="#111111" CREATED="1671670038965" ID="ID_757271454" MODIFIED="1671670094507" TEXT="COBIT (Control Objectives for Information and Related Technology)"/>
+<node COLOR="#111111" CREATED="1671670047225" ID="ID_630235597" MODIFIED="1671670149177" TEXT="NIST (National Institute of Standard and Technology)"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1671669309688" ID="ID_1809437801" MODIFIED="1671669328182" TEXT="Implementacion mediante mecanismos de seguridad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1671669341527" ID="ID_73867667" MODIFIED="1671669350071" TEXT="Fisicos - Hardware">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1671669353370" ID="ID_1293179761" MODIFIED="1671669376677" TEXT="L&#xf3;gicos - Software">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
