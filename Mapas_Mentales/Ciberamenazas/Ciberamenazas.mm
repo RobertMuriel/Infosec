e8fd3f6216cd084be61fa6983d056b644bf7dce7
RobertMuriel <116899445+RobertMuriel@users.noreply.github.com>
2023-01-02 08:33:33 -0600

    Nuevo mapa mental sobre Ciberamenazas

---
+++ Mapas_Mentales/Ciberamenazas/Ciberamenazas.mm
@@ -0,0 +1,75 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1672667919080" ID="ID_836117993" MODIFIED="1672669643615" TEXT="Ciberamenazas">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node BACKGROUND_COLOR="#cccccc" COLOR="#0033ff" CREATED="1672668331085" ID="ID_1298092295" MODIFIED="1672669643597" POSITION="left" TEXT="Actividades maliciosas que pueden da&#xf1;ar o interrumpir un sistema informattico, una red o la informaci&#xf3;n que contiene">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="idea"/>
+</node>
+<node COLOR="#0033ff" CREATED="1672668508069" ID="ID_721239197" MODIFIED="1672669643597" POSITION="right" TEXT="Tipos de Ciberamenazas - Clasificadas por su estructura">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-1"/>
+<node BACKGROUND_COLOR="#cccccc" COLOR="#00b439" CREATED="1672668805390" ID="ID_118448275" MODIFIED="1672669643605" TEXT="La estructura esta relacionada con los recursos, la organizaci&#xf3;n y la financiaci&#xf3;n del atacante">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1672668921398" ID="ID_615950213" MODIFIED="1672669643605" TEXT="Amenazas no estructuradas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1672669016248" ID="ID_925359520" MODIFIED="1672669643605" TEXT="Individuos o grupos peque&#xf1;os con poca o ninguna organizaci&#xf3;n y financiaci&#xf3;n">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669049010" ID="ID_247692912" MODIFIED="1672669643605" TEXT="Facilmente detectables">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669070412" ID="ID_1376613671" MODIFIED="1672669643605" TEXT="Eplotaci&#xf3;n basada en vulnerabilidades documentadasz">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669097357" ID="ID_1557079694" MODIFIED="1672669643605" TEXT="Ataques oportunistas">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669110814" ID="ID_1037389844" MODIFIED="1672669643605" TEXT="Motivacion: Diversion, sentimiento de orgullo, actividades de cibercrimen, etc...">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1672668945348" ID="ID_1381603982" MODIFIED="1672669643605" TEXT="Amenazas estructuradas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1672669189629" ID="ID_248625428" MODIFIED="1672669643605" TEXT="Bien organizado, planificado o financiado">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669208757" ID="ID_1870042649" MODIFIED="1672669643605" TEXT="Objetivo especifico y recopilaci&#xf3;n de informaci&#xf3;n extensa para elegir medios de ataque">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669259155" ID="ID_1571810951" MODIFIED="1672669643605" TEXT="Explotaci&#xf3;n con la ayuda de espertos o mediante vulnerabilidades no documentadas (Zero Days)">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669306199" ID="ID_1153826689" MODIFIED="1672669643615" TEXT="Ataques dirijidos a un objetivo concreto">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1672668958664" ID="ID_1767285703" MODIFIED="1672669643615" TEXT="Amenazas altamente estructuradas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1672669334515" ID="ID_423574322" MODIFIED="1672669643615" TEXT="Amplia organizaci&#xf3;n, financiaci&#xf3;n y planificacion a lo largo del tiempo">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669377837" ID="ID_1513945241" MODIFIED="1672669643615" TEXT="Objetivo a largo plazo que va m&#xe1;s all&#xe1; de los datos especificos o la m&#xe1;quina que esta siendo atacada">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669443153" ID="ID_929696669" MODIFIED="1672669643615" TEXT="Explotacion avanzada con m&#xfa;ltiples vectores (T&#xe9;cnico, sociales) y/o ayuda interna">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669504798" ID="ID_719006760" MODIFIED="1672669643615" TEXT="Posible esfuerzo coordinado de m&#xfa;ltiples grupos">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1672669530328" ID="ID_452088930" MODIFIED="1672669643615" TEXT="Relacionado con terminos como &quot;APT&quot; y &quot;Malware patrocinado por estados&quot;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
