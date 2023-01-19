edfdb06d38e653f38c0930b70365600c207838f0
rmuriel <rmuriel@iteso.mx>
2023-01-19 10:50:58 -0600

    Actualizacion Readme, Nuevo Mapa Mental Informe Tecnico Forense

---
+++ Mapas Mentales/Informe Tecnico Forense/Informe Técnico Forense.mm
@@ -0,0 +1,94 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1674143838068" ID="ID_1494349" MODIFIED="1674146261794" TEXT="Informe T&#xe9;cnico Forense">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1674143906393" ID="ID_599099535" MODIFIED="1674146271485" POSITION="right" TEXT="Resumen Ejecutivo">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-1"/>
+<node COLOR="#00b439" CREATED="1674144313769" ID="ID_1441811954" MODIFIED="1674146261778" TEXT="Uno o dos p&#xe1;rrafos que resuman su informe">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144341108" ID="ID_785693455" MODIFIED="1674146261783" TEXT="Contener una breve descripci&#xf3;n de elcaso investigado, la informaci&#xf3;n de los antecedentes y los proncipales hallazgos y concluciones">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674143921597" ID="ID_863386798" MODIFIED="1674146280150" POSITION="left" TEXT="Antecedentes y Objetivos">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-2"/>
+<node COLOR="#00b439" CREATED="1674144455443" ID="ID_1660035178" MODIFIED="1674146261784" TEXT="Toda la informaci&#xf3;n relevante respecto al contexto">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144495221" ID="ID_770869911" MODIFIED="1674146261786" TEXT="Descripcion de porque fueron necesarios los examenes forense de esas maquinas, dispositivos y/o datos">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144594748" ID="ID_1711406203" MODIFIED="1674146261787" TEXT="Resumen de todas las tareas realizadas en la investigaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674143933013" ID="ID_888864544" MODIFIED="1674146292409" POSITION="right" TEXT="Evidencias Analizadas">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-3"/>
+<node COLOR="#00b439" CREATED="1674144701431" ID="ID_1214958300" MODIFIED="1674146261788" TEXT="Todas las evidencias recopiladas y analizadas durante su investigaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144732632" ID="ID_1028769568" MODIFIED="1674146261789" TEXT="El uso de una tabla es apropiado">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674143943892" ID="ID_1971588259" MODIFIED="1674146299632" POSITION="left" TEXT="Hallazgos Relevantes y Detalles del Proceso">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-4"/>
+<node COLOR="#00b439" CREATED="1674144764855" ID="ID_205903892" MODIFIED="1674146261790" TEXT="Los principales hallazgos de la investigaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144785139" ID="ID_1811865147" MODIFIED="1674146261791" TEXT="Cada hallazgo debe venir acompa&#xf1;ado con un an&#xe1;lisis en profundidad">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674143993970" ID="ID_350418002" MODIFIED="1674146311778" POSITION="right" TEXT="Pistas Abiertas">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-5"/>
+<node COLOR="#00b439" CREATED="1674144845789" ID="ID_423485482" MODIFIED="1674146261792" TEXT="Tareas incompletas">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144852495" ID="ID_1233728570" MODIFIED="1674146261792" TEXT="Incluye evidencias que no se pudieron analizar">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674144003483" ID="ID_1918633708" MODIFIED="1674146319298" POSITION="left" TEXT="Conclusiones">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-6"/>
+<node COLOR="#00b439" CREATED="1674144894812" ID="ID_570869949" MODIFIED="1674146261793" TEXT="Resume los principales hallazgos t&#xe9;cnicos de la investigaci&#xf3;n">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1674144950020" ID="ID_1403213121" MODIFIED="1674146261794" TEXT="Expresar la opinion como experto forense">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1674144011887" ID="ID_1776906902" MODIFIED="1674146325389" POSITION="right" TEXT="Anexos">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<icon BUILTIN="full-7"/>
+</node>
+</node>
+</map>
--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ** Ciberseguridad, Seguridad de la Información, Privacidad, Proteccion de Datos Personales, Hacking, Pentesting **
 Robert Muriel           
 
+
 [Cheatsheets](Cheatsheets)
 
 - [DNS.jpg](Cheatsheets%2FDNS.jpg)
@@ -16,23 +17,31 @@ Robert Muriel
 - [SQL 2 of 2.jpg](Cheatsheets%2FSQL%202%20of%202.jpg)
 - [TCP-UDP Common Ports.jpg](Cheatsheets%2FTCP-UDP%20Common%20Ports.jpg)
 
+
 [Mapas Mentales](Mapas%20Mentales)
-
-- [HTML - Fundamentos_de_Ciberseguridad.mm.html](https://htmlpreview.github.io/?https://github.com/RobertMuriel/Infosec/blob/main/Mapas%20Mentales/Fundamentos%20de%20Ciberseguridad/Fundamentos_de_Ciberseguridad.mm.html)
+PDF
 - [PDF - Fundamentos_de_Ciberseguridad.pdf](Mapas%20Mentales%2FFundamentos%20de%20Ciberseguridad%2FFundamentos_de_Ciberseguridad.pdf)
-- [FreeMind - Ciberamenazas.mm](Mapas%20Mentales%2FCiberamenazas%2FCiberamenazas.mm)
-
-- [HTML - Ciberamenazas.mm.html](https://htmlpreview.github.io/?https://github.com/RobertMuriel/Infosec/blob/main/Mapas%20Mentales/Ciberamenazas/Ciberamenazas.mm.html)
 - [PDF - Ciberamenazas.pdf](Mapas%20Mentales%2FCiberamenazas%2FCiberamenazas.pdf)
-- [FreeMind - Ciberamenazas.pdf](Mapas%20Mentales%2FCiberamenazas%2FCiberamenazas.pdf)
+- [Informe Técnico Forense.pdf](Mapas%20Mentales%2FInforme%20Tecnico%20Forense%2FInforme%20T%E9cnico%20Forense.pdf)
+HTML
+- [HTML - Fundamentos_de_Ciberseguridad.mm.html](https://htmlpreview.github.io/?https://github.com/RobertMuriel/Infosec/blob/main/Mapas%20Mentales/Fundamentos%20de%20Ciberseguridad/Fundamentos_de_Ciberseguridad.mm.html)
+- [HTML - Ciberamenazas.mm.html](https://htmlpreview.github.io/?https://github.com/RobertMuriel/Infosec/blob/main/Mapas%20Mentales/Ciberamenazas/Ciberamenazas.mm.html)
+- [Informe Técnico Forense.mm.html](Mapas%20Mentales%2FInforme%20Tecnico%20Forense%2FInforme%20T%E9cnico%20Forense.mm.html)
+Fuente - Free Mind 
+- [Fundamentos_de_Ciberseguridad.mm](Mapas%20Mentales%2FFundamentos%20de%20Ciberseguridad%2FFundamentos_de_Ciberseguridad.mm)
+- [Ciberamenazas.mm](Mapas%20Mentales%2FCiberamenazas%2FCiberamenazas.mm)
+- [Informe Técnico Forense.mm](Mapas%20Mentales%2FInforme%20Tecnico%20Forense%2FInforme%20T%E9cnico%20Forense.mm)
+
 
 [Notas Hacking - Pentesting](Notas%20Hacking%20-%20Pentesting)
 
-- [1.- Reconocimiento - Reconnaissance](Notas%20Hacking%20-%20Pentesting%2F1.-%20Reconocimiento%20-%20Reconnaissance)
-- [2.- Descubrimiento - Discovery](Notas%20Hacking%20-%20Pentesting%2F2.-%20Descubrimiento%20-%20Discovery)
-- [3.- Analisis de Vulnerbilidades - Vulnerability Assessment](Notas%20Hacking%20-%20Pentesting%2F3.-%20Analisis%20de%20Vulnerbilidades%20-%20Vulnerability%20Assessment)
-- [4.- Explotacion - Exploitation](Notas%20Hacking%20-%20Pentesting%2F4.-%20Explotacion%20-%20Exploitation)
-- [5.- Escalada de Privilegios - Privilege Escalation](Notas%20Hacking%20-%20Pentesting%2F5.-%20Escalada%20de%20Privilegios%20-%20Privilege%20Escalation)
+- [1.- Reconocimiento.txt](Notas%20Hacking%20-%20Pentesting%2F1.-%20Reconocimiento%20-%20Reconnaissance%2FReconocimiento.txt)
+- [2.- Descubrimiento.txt](Notas%20Hacking%20-%20Pentesting%2F2.-%20Descubrimiento%20-%20Discovery%2FDescubrimiento.txt)
+- [3.- Analisis de Vulnerabilidades.txt](Notas%20Hacking%20-%20Pentesting%2F3.-%20Analisis%20de%20Vulnerbilidades%20-%20Vulnerability%20Assessment%2FAnalisis%20de%20Vulnerabilidades.txt)
+- [4.- Explotacion.txt](Notas%20Hacking%20-%20Pentesting%2F4.-%20Explotacion%20-%20Exploitation%2FExplotacion.txt)
+- [5.- Escalada de Privilegios.txt](Notas%20Hacking%20-%20Pentesting%2F5.-%20Escalada%20de%20Privilegios%20-%20Privilege%20Escalation%2FEscalada%20de%20Privilegios.txt)
+- [6.- Miscelanius.txt](Notas%20Hacking%20-%20Pentesting%2FMisc%2FMiscelanius.txt)
+
 
 [Pivoting](Pivoting)
 - [NetScan.sh -> Bash Script for network scanning with PING from pivot machine](Pivoting/NetScan.sh)
